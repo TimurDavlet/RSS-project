f19ded6c4cd93b37ef532a7e457e31a6e71e4d86
TimurDavlet <timkoootim@yandex.ru>
2022-02-14 05:34:08 +0400

    add i18n

---
+++ src/js/locales/ru.js
@@ -0,0 +1,12 @@
+export default {
+  translation: {
+    languages: {
+      en: 'English',
+      ru: 'Русский',
+    },
+    errors: {
+      url: 'Ссылка должна быть валидным URL',
+      created: 'RSS уже существует',
+    },
+  },
+};
--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,8 @@ import * as yup from 'yup';
 import isEmpty from 'lodash/isEmpty.js';
 import has from 'lodash/has.js';
 import { keyBy } from 'lodash-es';
+import i18n from 'i18next';
+import resources from './locales/index.js';
 import render from './view.js';
 //import url from 'url';
 
@@ -17,43 +19,59 @@ const elements = {
   error: document.querySelector('[class="feedback m-0 position-absolute small text-danger"]'),
 };
 
-const schema = yup.object().shape({
-  website: yup.string().url().nullable(),
-});
+// export default
+const start = () => {
+  const defaultLanguage = 'ru';
+  // каждый запуск приложения создаёт свой собственный объект i18n и работает с ним,
+  // не меняя глобальный объект.
+  const i18nInstance = i18n.createInstance();
+  i18nInstance.init({
+    lng: defaultLanguage,
+    debug: false,
+    resources,
+  });
 
-const validate = (value) => {
-  const valid = schema.validate({ website: value }, { abortEarly: false });
-  return valid.then(() => {
-    const result = {};
-    return result;
-  }).catch((e) => keyBy(e.inner, 'path'));
+  const state = onChange({
+    formRSS: {
+      listOfFeeds: [],
+      errors: '',
+      processState: '',
+      processError: null,
+    },
+  }, render(elements));
+
+  yup.setLocale({
+    string: {
+      url: i18nInstance.t('errors.url'),
+    },
+    mixed: {
+      notOneOf: i18nInstance.t('errors.created'),
+    },
+  });
+
+  const schema = yup.object().shape({
+    website: yup.string().url().notOneOf([state.formRSS.listOfFeeds]),
+  });
+
+  const validate = (value) => {
+    const valid = schema.validate({ website: value }, { abortEarly: false });
+    return valid.then(() => {
+      const result = {};
+      return result;
+    });
+  };
+
+  elements.form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const value = formData.get('url');
+    const error = validate(value, state.formRSS.listOfFeeds);
+    error.then(() => {
+      state.formRSS.listOfFeeds.push(value);
+    }).catch((e) => {
+      state.formRSS.errors = e.errors;
+    });
+  });
 };
 
-const state = onChange({
-  formRSS: {
-    listOfFeeds: [],
-    errors: '',
-    processState: '',
-    processError: null,
-  },
-}, render(elements));
-
-elements.form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const formData = new FormData(e.target);
-  const value = formData.get('url');
-  const error = validate(value);
-  const isFieldInFeeds = state.formRSS.listOfFeeds.includes(value);
-  error.then((data) => {
-    if (isEmpty(data) && !isFieldInFeeds) {
-      state.formRSS.listOfFeeds.push(value);
-      // передал в render
-      // что-то отрисовываем в render по пути formRSS.listOfFeeds.
-      console.log('валидация прошла успешно');
-    } else {
-      state.formRSS.errors = isFieldInFeeds ? 'RSS уже существует' : 'Ссылка должна быть валидным URL';
-      // передал в render
-      // что-то отрисовываем в render по пути formRSS.errors.
-    }
-  });
-});
+start();
