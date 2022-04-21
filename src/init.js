87f92657eae6b46a5f5f03ad3ec910d6e1b68205
TimurDavlet <timkoootim@yandex.ru>
2022-04-21 20:03:32 +0400

    init

--- src/init.js
+++ src/init.js
@@ -6,36 +6,37 @@ import render from './render.js';
 import view from './view.js';
 
 export default () => {
-  const elements = {
-    feedback: document.querySelector('.feedback'),
-    feeds: document.querySelector('.feeds'),
-    form: document.querySelector('.rss-form'),
-    input: document.getElementById('url-input'),
-    posts: document.querySelector('.posts'),
-    submitButton: document.querySelector('button[type="submit"]'),
-  };
-  const state = onChange({
-    lng: 'ru',
-    links: [],
-    input: {
-      readonly: false,
-    },
-    feedback: {
-      error: null,
-      success: null,
-    },
-    valid: true,
-    newFeed: [],
-    newPosts: [],
-    feeds: [],
-    posts: [],
-  }, render(elements));
-
   const i18nextInstance = i18next.createInstance();
   i18nextInstance.init({
-    lng: state.lng,
+    lng: 'ru',
     resources,
-  }).then(() => view(elements, state, i18nextInstance));
+  }).then(() => {
+    const elements = {
+      feedback: document.querySelector('.feedback'),
+      feeds: document.querySelector('.feeds'),
+      form: document.querySelector('.rss-form'),
+      input: document.getElementById('url-input'),
+      posts: document.querySelector('.posts'),
+      submitButton: document.querySelector('button[type="submit"]'),
+    };
 
-  //view(elements, state, i18nextInstance);
+    const state = onChange({
+      lng: 'ru',
+      links: [],
+      input: {
+        readonly: false,
+      },
+      feedback: {
+        error: null,
+        success: null,
+      },
+      valid: true,
+      newFeed: [],
+      newPosts: [],
+      feeds: [],
+      posts: [],
+    }, render(elements));
+
+    view(elements, state, i18nextInstance);
+  });
 };
