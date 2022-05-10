f53b3aee5f6d0559edc48db16cb994ba5c04dbe8
TimurDavlet <timkoootim@yandex.ru>
2022-05-11 02:00:18 +0400

    getnewpost

--- src/view.js
+++ src/view.js
@@ -21,17 +21,16 @@ const makeRequest = (i18n, link) => axios.get(routes.allOrigins(link))
   });
 
 const getNewPost = (state, i18n) => {
-  const promises = state.links.map((link) => makeRequest(i18n, link));
-  Promise.all(promises).then((data) => {
-    data.forEach((element) => {
-      const newFeed = parser(element.contents, state.feedback, i18n);
+  const promises = state.links.map((link) => makeRequest(i18n, link)
+    .then((data) => {
+      const newFeed = parser(data.contents, state.feedback, i18n);
       const newPosts = _.differenceBy(newFeed.feedItems, state.posts, 'postLink');
       if (newPosts.length > 0) {
         state.newPosts = [...newPosts];
         state.posts = [...newPosts, ...state.posts];
       }
-    });
-  }).finally(setTimeout(() => getNewPost(state, i18n), 5000));
+    }));
+  Promise.all(promises).finally(setTimeout(() => getNewPost(state, i18n), 5000));
 };
 
 const getFeeds = (state, i18n, link) => makeRequest(i18n, link)
