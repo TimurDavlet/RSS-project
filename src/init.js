af5fdd7668759b504e8c4a6825fdbb953c7fa86b
TimurDavlet <timkoootim@yandex.ru>
2022-07-05 12:34:22 +0400

    state

--- src/init.js
+++ src/init.js
@@ -1,9 +1,12 @@
+/* eslint-disable no-shadow */
+/* eslint-disable no-param-reassign */
+/* eslint-disable array-callback-return */
 import i18next from 'i18next';
 import onChange from 'on-change';
+import axios from 'axios';
+import _ from 'lodash';
 import resources from './locales/index.js';
 import render from './view.js';
-import _ from 'lodash';
-import axios from 'axios';
 import validate from './validation.js';
 import parser from './parser.js';
 import 'bootstrap';
@@ -19,70 +22,70 @@ export default () => {
     modal: document.querySelector('#modal'),
   };
 
-const routes = {
-  allOrigins: (url) => {
-    const result = new URL('/get', 'https://allorigins.hexlet.app');
-    result.searchParams.set('url', url);
-    result.searchParams.set('disableCache', 'true');
-    return result.toString();
-  },
-};
+  const routes = {
+    allOrigins: (url) => {
+      const result = new URL('/get', 'https://allorigins.hexlet.app');
+      result.searchParams.set('url', url);
+      result.searchParams.set('disableCache', 'true');
+      return result.toString();
+    },
+  };
 
-const getNewPost = (state) => {
-  const promises = state.links.map((link) => {
-    axios.get(routes.allOrigins(link))
-      .then((response) => response.data)
-      .then((data) => {
-        const newFeed = parser(data.contents, state);
-        const newPosts = _.differenceBy(newFeed.feedItems, state.posts, 'postLink');
-        if (newPosts.length > 0) {
-          state.newPosts = [...newPosts];
-          state.posts = [...newPosts, ...state.posts];
-        }
-      });
-  });
-  Promise.all(promises).finally(setTimeout(() => getNewPost(state), 5000));
-};
+  const getNewPost = (state) => {
+    const promises = state.links.map((link) => {
+      axios.get(routes.allOrigins(link))
+        .then((response) => response.data)
+        .then((data) => {
+          const newFeed = parser(data.contents, state);
+          const newPosts = _.differenceBy(newFeed.feedItems, state.posts, 'postLink');
+          if (newPosts.length > 0) {
+            state.newPosts = [...newPosts];
+            state.posts = [...newPosts, ...state.posts];
+          }
+        });
+    });
+    Promise.all(promises).finally(setTimeout(() => getNewPost(state), 5000));
+  };
 
-const view = (elements, state) => {
-  elements.form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const link = elements.input.value;
-    state.processState = 'loading';
-    validate(link, state.links)
-      .then(() => axios.get(routes.allOrigins(link)))
-      .then((response) => response.data)
-      .then((data) => {
-        const feed = parser(data.contents);
-        const post = feed.feedItems;
-        state.feeds = [...state.feeds, feed];
-        state.links.push(link);
-        state.posts = [...state.posts, ...post];
-        state.newPosts = post;
-        state.processState = 'success';
-      })
-      .catch((err) => {
-        state.processState = 'Error';
-        if (err.isAxiosError) {
-          state.feedback.error = 'netError';
-        } else if (err.isParsingError) {
-          state.feedback.error = 'parseError';
-        } else if (err.name === 'ValidationError') {
-          const massage = err.message;
-          state.feedback.error = massage;
-        } else {
-          console.log('Unknown Error');
-        }
-      });
-  });
-  elements.posts.addEventListener('click', (event) => {
-    const currentId = event.target.dataset.id;
-    if (currentId !== undefined) {
-      state.visitedPostsId.add(currentId);
-      state.currentPostId = {posts: state.posts, currentId};
-    }
-  });
-};
+  const view = (elements, state) => {
+    elements.form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      const link = elements.input.value;
+      state.processState = 'loading';
+      validate(link, state.links)
+        .then(() => axios.get(routes.allOrigins(link)))
+        .then((response) => response.data)
+        .then((data) => {
+          const feed = parser(data.contents);
+          const post = feed.feedItems;
+          state.feeds = [...state.feeds, feed];
+          state.links.push(link);
+          state.posts = [...state.posts, ...post];
+          state.newPosts = post;
+          state.processState = 'success';
+        })
+        .catch((err) => {
+          state.processState = 'Error';
+          if (err.isAxiosError) {
+            state.feedback.error = 'netError';
+          } else if (err.isParsingError) {
+            state.feedback.error = 'parseError';
+          } else if (err.name === 'ValidationError') {
+            const massage = err.message;
+            state.feedback.error = massage;
+          } else {
+            console.log('Unknown Error');
+          }
+        });
+    });
+    elements.posts.addEventListener('click', (event) => {
+      const currentId = event.target.dataset.id;
+      if (currentId !== undefined) {
+        state.visitedPostsId.add(currentId);
+        state.currentPostId = { posts: state.posts, currentId };
+      }
+    });
+  };
 
   const i18nextInstance = i18next.createInstance();
 
